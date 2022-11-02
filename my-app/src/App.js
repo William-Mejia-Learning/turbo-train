b45b4233bc66918b8b847c6d11d5c2cf1201608a
williamxmejia <willx1995@gmail.com>
2022-11-01 21:44:56 -0500

    feat:footer is at the bottom, still needs some changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import Footer from './components/Footer'
 import Landing from './components/Landing'
+import "./bootstrap.css"
+import "./style.css"
 
 function App() {
   return (
     <div>
-      <div>
+      <div id="page-container">
         <Landing></Landing>
-        <Footer></Footer>
       </div>
-
+      <Footer id="footer"></Footer>
     </div>
   );
 }
